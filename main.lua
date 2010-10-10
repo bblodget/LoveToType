56cddfd2844f69d098b5a9a55469afbaddeb6428
Brandon Blodget <brandon.blodget@gmail.com>
2010-10-10 16:06:12 -0700

    Added the ability to toggle fullscreen on or off.
    Use the "escape" key to toggle.

--- main.lua
+++ main.lua
@@ -5,6 +5,10 @@ function love.load()
 	font2 = love.graphics.newFont("Miama/Miama.ttf",24)
 	love.graphics.setBackgroundColor(128,128,255) -- light blue
 	love.graphics.setColor(0,0,0)  -- black
+    love.graphics.setCaption("Touch Typing Tutor")
+	
+	changed = false
+	full_screen = false
 end
 
 function love.draw()
@@ -17,7 +21,28 @@ function love.draw()
 				280,220,800, 'left')
 
 	love.graphics.printf("Your Choice? ",400,400,800, 'left')
+end
 
+
+function love.keypressed(key, unicode)
+	if key == "escape" then
+		if (not full_screen) then
+			-- change to full screen
+			changed = love.graphics.toggleFullscreen()
+			if (changed) then
+				full_screen = true
+			end
+		else
+			-- change to window mode
+			changed = love.graphics.setMode(800,600,false,true)
+			if (changed) then
+				full_screen = false
+			end
+		end
+	end
+
+	if key == "q" then
+	end
 
 end
 
