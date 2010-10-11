9f245031bb657d6dbff91c9a11b78be4b236ee5f
Brandon Blodget <brandon.blodget@gmail.com>
2010-10-10 17:11:25 -0700

    Added full/window screen and quit as numbered choices.

--- main.lua
+++ main.lua
@@ -15,37 +15,58 @@ function love.draw()
 	love.graphics.setFont(font)
 
 	love.graphics.printf("Touch Typing Tutor",0,120,800, 'center')
-	love.graphics.printf("1. Lessons\n" .. 
-						 "2. Diagnostic\n" ..
-						 "3. Game\n", 
-				280,220,800, 'left')
 
-	love.graphics.printf("Your Choice? ",400,400,800, 'left')
+	if (not full_screen) then
+		love.graphics.printf("1. Lessons\n" .. 
+							 "2. Diagnostic\n" ..
+							 "3. Game\n" ..
+							 "4. Full Screen\n" ..
+							 "5. Quit\n", 
+					280,220,800, 'left')
+	else
+		love.graphics.printf("1. Lessons\n" .. 
+							 "2. Diagnostic\n" ..
+							 "3. Game\n" ..
+							 "4. Windowed Screen\n" ..
+							 "5. Quit\n",
+					280,220,800, 'left')
+	end
+
+	love.graphics.printf("Your Choice? ",400,450,800, 'left')
 end
 
+function fullScreen()
+	if (not full_screen) then
+		-- change to full screen
+		changed = love.graphics.toggleFullscreen()
+		if (changed) then
+			full_screen = true
+		end
+	end
+end
+
+function windowScreen()
+	-- change to window mode
+	changed = love.graphics.setMode(800,600,false,true)
+	if (changed) then
+		full_screen = false
+	end
+end
 
 function love.keypressed(key, unicode)
-	if key == "escape" then
+	if key == "4" then
 		if (not full_screen) then
-			-- change to full screen
-			changed = love.graphics.toggleFullscreen()
-			if (changed) then
-				full_screen = true
-			end
+			fullScreen()
 		else
-			-- change to window mode
-			changed = love.graphics.setMode(800,600,false,true)
-			if (changed) then
-				full_screen = false
-			end
+			windowScreen()
 		end
 	end
 
-	if key == "q" then
+	if key == "5" then
 		-- quit the program
+		windowScreen()
 		love.event.push('q')
 	end
-
 end
 
 
